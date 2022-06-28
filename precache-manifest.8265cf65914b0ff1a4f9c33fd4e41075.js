21a8933b6fdefbbcd315689f3a5607d4dbc85ece
dimachilaia <92372285+dimachilaia@users.noreply.github.com>
2022-06-28 23:10:36 +0400

    Updates

---
+++ precache-manifest.8265cf65914b0ff1a4f9c33fd4e41075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af873b90808660875aaa9bccafaf7a3",
+    "url": "/ReactJs-ExponseTracker/index.html"
+  },
+  {
+    "revision": "cf5a96721da85a055554",
+    "url": "/ReactJs-ExponseTracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/ReactJs-ExponseTracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactJs-ExponseTracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5a96721da85a055554",
+    "url": "/ReactJs-ExponseTracker/static/js/main.790ad1b4.chunk.js"
+  },
+  {
+    "revision": "f1948334548b68983e0d",
+    "url": "/ReactJs-ExponseTracker/static/js/runtime-main.82cde1e1.js"
+  }
+]);
